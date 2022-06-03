67000c142ea14e2ea66cd0b55346fce4f17bed39
Carolina Nonato <csnonato@gmail.com>
2022-06-02 23:11:23 -0400

    post component

---
+++ src/Data/PostsData.js
@@ -0,0 +1,27 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.JPG'
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Tzuru',
+        desc: "Happy New Year friends! #2023",
+        likes: 2300,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Pipa',
+        desc: "Party Time",
+        likes: 210,
+        liked: false
+    },
+    {
+        img: postPic3,
+        name: 'Dog',
+        desc: "AU AU AU",
+        likes: 2800,
+        liked: false
+    }
+]   
