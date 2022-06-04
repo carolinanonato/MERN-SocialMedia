53c93b02d36643b0cc248f07b8c041023ae31f53
Carolina Nonato <csnonato@gmail.com>
2022-06-03 21:59:32 -0400

    modal popups - 1st part frontend done

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './InfoCard.css'
 import {UilPen} from '@iconscout/react-unicons'
+import ProfileModal from '../ProfileModal/ProfileModal'
 
 const InfoCard = () => {
+
+  const [modalOpened, setModalOpened] = useState(false)
+
   return (
     <div className="infoCard">
         <div className="infoHead">
             <h4>Your Info</h4>
-            <div>  <UilPen wigth='2rem' height='1.2rem'/></div>
+            <div>  <UilPen wigth='2rem' height='1.2rem' onClick={()=>setModalOpened(true)}/>
+            <ProfileModal modalOpened={modalOpened} setModalOpened={setModalOpened}/>
+            </div>
       
         </div>
         <div className="info">
