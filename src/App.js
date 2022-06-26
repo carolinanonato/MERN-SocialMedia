ffa3b190aa9a1577084e5aa6841dc11b3e3965e7
Carolina Nonato <csnonato@gmail.com>
2022-06-26 11:50:11 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css"
 import Auth from "./pages/Auth/Auth";
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useSelector } from "react-redux";
+import Home from "./pages/Home";
 
 function App() {
+  const user = useSelector((state) => state.authReducer.authData)
   return (
     <div className="App">
       <div className="blur" style={{ top: '-18%', right: '0' }}></div>
       <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
+      <Routes>
+        <Route path="/" element={user ? <Navigate to="home" /> : <Navigate to='auth/' />} />
+        <Route path='/home' element={user ? <Home /> : <Navigate to='../auth/' />} />
 
-      <Auth />
+        <Route path='/auth' element={user ? <Navigate to="../home" /> : <Auth />} />
+      </Routes>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Home.css'
-import ProfileSide from '../../components/profileSide/profileSide'
-import PostSide from '../../components/PostSide/PostSide'
-import RightSide from '../../components/RightSide/RightSide'
+import ProfileSide from '../components/profileSide/profileSide'
+import PostSide from '../components/PostSide/PostSide'
+import RightSide from '../components/RightSide/RightSide'
 
 
 
